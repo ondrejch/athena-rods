c2bb2a029097925c0f66f05bb56c9f3afc0c4773
Ondrej Chvala <ondrejch@gmail.com>
2025-09-09 00:34:30 -0400

    correct path

--- src/arod_control/__init__.py
+++ src/arod_control/__init__.py
@@ -1,6 +1,6 @@
 # ATHENA-rods communication setup
 USE_SSL = True                              # Use SSL in communication
-AUTH_ETC_PATH: str = "git/athena_rods/etc"  # Path to CtrBox configuration, in home directory
+AUTH_ETC_PATH: str = "git/athena-rods/etc"  # Path to CtrBox configuration, in home directory
 PORT_STREAM = 65432             # Port for streaming data
 PORT_CTRL = 65433               # Port for control data
 CONTROL_IP = '192.168.1.56'     # IP of the CtrBox machine
